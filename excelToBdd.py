b7e89ce5f1332184d99887cc42dee77d8bc689f3
aurelien beliard <aurelien.beliard@laposte.net>
2017-08-10 14:11:29 +0200

    reformat dict and date

--- excelToBdd.py
+++ excelToBdd.py
@@ -1,17 +1,23 @@
 import xlrd
-import sys
-from collections import OrderedDict
-import simplejson as json
-import pymongo
 from pymongo import MongoClient
 import re
 import glob2
 import logging
+from log import *
+
 client = MongoClient('localhost', 27017)
 db = client.pymongo_test
+client.drop_database(db)
 
+def checkDates(sheet: xlrd.sheet.Sheet, cellTmp: int, cellNumber: int,cellLine: int) ->list:
 
-def checkDates(sheet, cellTmp, cellNumber,cellLine):
+    """
+    :param sheet:
+    :param cellTmp:
+    :param cellNumber:
+    :param cellLine:
+    :return:
+    """
     matchc = re.match(r"(.*):\s*(\d{2}\/\d{2}\/\d{4})", cellTmp)
     if re.match(r"(.*):\s*(\d{2}\/\d{2}\/\d{4})", cellTmp):
         val_content = matchc.group(1)
@@ -20,21 +26,22 @@ def checkDates(sheet, cellTmp, cellNumber,cellLine):
     elif re.match(r"([a-zA-Zéè]{4}\s[a-zA-Zéè]{2}\s[a-zA-Zéè]{10}\s[a-zA-Zéè]{8}:)", cellTmp):
         matchn = re.match(r"([a-zA-Zéè]{4}\s[a-zA-Zéè]{2}\s[a-zA-Zéè]{10}\s[a-zA-Zéè]{8}:)", cellTmp)
         val_content = matchn.group(1)
-        print ("ihihih")
 
-        datec = sheet.cell(0, cellNumber + 2)
-        print("try again")
-        print(datec.value)
+        datec = sheet.cell(cellLine, cellNumber + 2)
 
-        if re.match(r"(\d{2}\/\d{2}\/\d{4})", str(datec.value)):
-            dateMatch = re.match(r"(\d{2}\/\d{2}\/\d{4})", datec.value)
-            date = dateMatch.group(1)
-            return [val_content,date]
+        if xlrd.xldate_as_tuple(datec.value, 0):
+            date = xlrd.xldate_as_tuple(datec.value, 0)
+            str_date=str(date[2])+"/"+str(date[1])+"/"+str(date[0])
+            return [val_content,str_date]
         else:
-            raise ValueError("no valuable date ")
+            raise ValueError("no normal date ")
 
 
-def get_file():
+def get_file()->list:
+    """
+
+    :return:
+    """
     files =glob2.glob("/home/aurelien/test_import_BDD/*.xls")
     if len(files)==0:
         raise Exception("file not found")
@@ -43,72 +50,82 @@ def get_file():
 file_list = get_file()
 
 
-def extract_content(file):
+def extract_content(file: str)->dict:
+    """
+    :param file:
+    :return:
+    """
     book = xlrd.open_workbook(file , formatting_info=True)
-    print("extcont"+file)
     sheets = book.sheet_names()
     patient= {}
-    print ("sheets are:", sheets)
     for index, sh in enumerate(sheets):
         sheet = book.sheet_by_index(index)
-        print ("Sheet:", sheet.name)
-        print(sheet)
         rows, cols = sheet.nrows, sheet.ncols
         if rows>0 and cols>0:
-            print ("Number of rows: %s   Number of cols: %s" % (rows, cols))
-            print(sheet.cell(0,0).value)
             patient["name"] = sheet.cell(0,0).value
-            patient["barcode"] = sheet.cell(0, 2).value
-            patient["samplename"] = sheet.cell(0,3).value
+            if sheet.cell(0, 2).value!='':
+                patient["barcode"] = sheet.cell(0, 2).value
+                patient["samplename"] = sheet.cell(0, 3).value
+            elif  sheet.cell(0, 3).value!='':
+                patient["barcode"] = sheet.cell(0, 3).value
+                patient["samplename"] = sheet.cell(0, 4).value
+            else :
+                logging.info("problem in %s"%(file))
+
             val_content=""
             date=""
             for cellnumber in range(7, 9):
                 celld = sheet.cell(0, cellnumber).value
-                print(celld)
-                print ("test")
                 if checkDates(sheet, celld,cellnumber,0):
                     patient [checkDates(sheet, celld,cellnumber,0)[0]] = checkDates(sheet, celld,cellnumber,0)[1]
                 cellt = sheet.cell(1,cellnumber).value
                 if checkDates(sheet, cellt, cellnumber,1):
                     patient[checkDates(sheet, cellt,cellnumber,1)[0]] = checkDates(sheet, cellt,cellnumber,1)[1]
-
+            genlist=[]
             for row in range (rows):
                 if re.match(r"Couverture Moy/ Amp",str(sheet.cell(row,0).value)):
                     for rownumber in range(row, row+8):
-                        for col in range(cols):
-                            if type(sheet.cell(rownumber,col).value) == str:
-                                tab = sheet.cell(rownumber, col).value.split("\n")
-                                if len(tab)==3:
-                                    patient[tab[0]]=[tab[1],tab[2]]
+                        for col in range(0, cols):
+                            if type(sheet.cell(rownumber,col).value) == str and sheet.cell(rownumber,col).value!="":
+                                tab = sheet.cell(rownumber, col).value
+                                genlist.append(tab)
+                                patient["genelist"]=genlist
+
                 if re.match(r"Résultats JSI", str(sheet.cell(row, 0).value)):
 
                     for rownumber2 in range(row+2, rows):
                         mutationValueList = []
                         for col in range(1,cols):
-                            xfx = sheet.cell_xf_index(rownumber2, col)
-                            xf = book.xf_list[xfx]
-                            bgx = xf.background.pattern_colour_index
-                            if bgx!=64:
-                                mutationValueList.append(bgx)
-                            mutationValueList.append(sheet.cell(rownumber2,col).value)
-                            print(sheet.cell(rownumber2, 0).value)
-                            xfx = sheet.cell_xf_index(rownumber2, col)
-                            xf = book.xf_list[xfx]
-                            bgx = xf.background.pattern_colour_index
-                        patient[sheet.cell(rownumber2, 0).value]=mutationValueList
-            print(patient["name"])
-            print(patient)
-            return patient
-patients=[]
-print(get_file())
-for file in get_file():
-    print("boucle "+file)
-    patients.append(extract_content(file))
-        
-posts = db.posts
-result = posts.insert_many(patients)
-print("the end ")
-#print('One post: {0}'.format(result.inserted_id))
+#                            xfx = sheet.cell_xf_index(rownumber2, 1)
+ #                           xf = book.xf_list[xfx]
+  #                          bgx = xf.background.pattern_colour_index
+   #                         if bgx!=64:
 
-test_post = posts.find_one({"samplename": "L1706221"})
-print(test_post)
+                              #  mutationValueList.append(bgx)
+                            if sheet.cell(rownumber2,col).value!="":
+                                mutationValueList.append(sheet.cell(rownumber2,col).value)
+#
+                        patient[sheet.cell(rownumber2, 0).value]=mutationValueList
+
+            logging.info(patient)
+            return patient
+
+if __name__ == '__main__':
+    logging.info("beginning of analysis")
+    logging.info("beginning of data extraction")
+    patients=[]
+    for file in get_file():
+        patients.append(extract_content(file))
+        logging.info("extracting %s file"%file)
+        
+    posts = db.posts
+    logging.info("adding in the database")
+    result = posts.insert_many(patients)
+
+    test_post = posts.find_one({"samplename": "L1706268"})
+    test_post2 = posts.find_one({"samplename": "L1706263"})
+    test_post3 = posts.find_one({"samplename": "L1706269"})
+    print(test_post)
+    print (test_post2)
+    print(test_post3)
+    logging.info("end of the analysis")
